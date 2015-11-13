d239800da06959ac4bb4b57665d8e77f96b4d30b
meisio <hello@meis.io>
2015-11-13 19:44:07 +0100

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apifyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the apifyApp
+ */
+angular.module('apifyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apifyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apifyApp
+ */
+angular.module('apifyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
